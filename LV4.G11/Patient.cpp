0d4167bb5c1056eda88da5372f28ebd7fff4f51f
dylsey <dmyelenich@gmail.com>
2024-06-21 11:17:30 -0500

    commit before work.

---
+++ LV4.G11/Patient.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include "Patient.h"
+
+int Patient::lastID{ 1 };
+
+Patient::Patient()
+	: ID{ lastID }
+{
+	lastID++;
+
+	std::cout << "What is the patient's first name: " << std::endl;
+	std::cin >> firstName;
+
+	std::cout << "What is the patient's last name: " << std::endl;
+	std::cin >> lastName;
+
+	std::cout << "What level would you rate teh patient's condition: \n"
+		<< "\t1. mild\n"
+		<< "\t2. moderate\n"
+		<< "\t3. emergency\n";
+
+	int userInput{};
+	std::cin >> userInput;
+
+	rating = ((Patient::Level)(userInput - 1));
+}
+
+Patient::Patient(std::string firstName, std::string lastName, Level rating)
+	:firstName{ firstName }, lastName{ lastName }, rating{ rating }
+{
+}
+
+
+void Patient::treatPatient()
+{
+	std::cin.ignore();
+	std::cout << "What is the patient's condition: ";
+	std::getline(std::cin, condition);
+
+	std::cout << "What is the treatment recommendation: ";
+	std::getline(std::cin, treatment);
+}
+
+void Patient::printPatient() const
+{
+	system("CLS");
+	std::cout << firstName << " " << lastName << std::endl;
+	std::cout << "ID: " << ID << std::endl;
+	std::cout << "Condition: " << condition << std::endl;
+	std::cout << "Treatment: " << treatment << std::endl;
+	system("PAUSE");
+
+}
+
+bool Patient::matchIDs(int key) const
+{
+	return key == ID;
+}
--- LV4.G11/Source.cpp
+++ LV4.G11/Source.cpp
@@ -2,38 +2,17 @@
 #include <string>
 #include <vector>
 
-class Patient
-{
-public:
-	enum class Level { mild, moderate, emergency };
-	Patient();
-	void newPatient(int& lastId);
-	void treatPatient();
-	void printPatient() const;
+#include "Patient.h"
 
-	bool matchIDs(int key) const;
-	static int lastID;
-private:
-	int ID{};
-	std::string firstName;
-	std::string lastName;
-	Level rating{ Level::mild };
 
-	std::string condition;
-	std::string treatment;
 
-};
-
-int Patient::lastID{ 1 };
 Patient& findPatient(std::vector<Patient>& patients);
 
 int main()
 {
 	std::vector<Patient> patients;
-	const Patient patient;
-
-	int lastId{ 0 };
 	int userInpt{ 0 };
+
 
 	while (userInpt != 4)
 	{
@@ -49,8 +28,7 @@ int main()
 		switch (userInpt)
 		{
 		case 1:
-			patients.push_back(Patient());
-			patients.back().newPatient(lastId);
+			patients.push_back(Patient("Mr.", "Orme", Patient::Level::mild));
 			break;
 		case 2:
 			findPatient(patients).treatPatient();
@@ -62,55 +40,10 @@ int main()
 	}
 }
 
-Patient::Patient()
-	: ID{lastID}
-
-{
-	lastID++;
-
-	std::cout << "What is the patient's first name: " << std::endl;
-	std::cin >> firstName;
-
-	std::cout << "What is the patient's last name: " << std::endl;
-	std::cin >> lastName;
-
-	std::cout << "What level would you rate teh patient's condition: \n"
-		<< "\t1. mild\n"
-		<< "\t2. moderate\n"
-		<< "\t3. emergency\n";
-
-	int userInput{};
-	std::cin >> userInput;
-
-	rating = ((Patient::Level)(userInput - 1));
-}
 
 
-void Patient::treatPatient()
-{
-	std::cin.ignore();
-	std::cout << "What is the patient's condition: ";
-	std::getline(std::cin, condition);
 
-	std::cout << "What is the treatment recommendation: ";
-	std::getline(std::cin, treatment);
-}
 
-void Patient::printPatient() const
-{
-	system("CLS");
-	std::cout << firstName << " " << lastName << std::endl;
-	std::cout << "ID: " << ID << std::endl;
-	std::cout << "Condition: " << condition << std::endl;
-	std::cout << "Treatment: " << treatment << std::endl;
-	system("PAUSE");
-
-}
-
-bool Patient::matchIDs(int key) const
-{
-	return key == ID;
-}
 
 Patient& findPatient(std::vector<Patient>& patients)
 {
